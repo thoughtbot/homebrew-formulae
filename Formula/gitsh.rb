9eb48500dcabf2ec3d3efce5673afb31285d256c
George Brocklehurst <george@georgebrock.com>
2013-11-22 16:01:09 +0100

    Skip homebrew ruby dependency if system ruby >=2.0
    Homebrew runs with a modified path, so this should avoid depending on a
    rvm/rbenv ruby, but allow us to depend on system ruby if it's new enough.

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -5,7 +5,10 @@ class Gitsh < Formula
   url 'http://thoughtbot.github.io/gitsh/gitsh-0.1.tar.gz'
   sha1 '42d4fb2e4f0db4d54fdc47a61bfdfbcf4294682d'
 
-  depends_on 'Ruby'
+  system_ruby_version = `ruby -e "puts RUBY_VERSION"`.chomp
+  if system_ruby_version < '2.0.0'
+    depends_on 'Ruby'
+  end
 
   def install
     system "./configure", "--disable-debug",
