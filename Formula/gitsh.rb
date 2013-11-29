a5bf1bc6e4bdd6e9e3e85bff2dc92a805f903927
George Brocklehurst <george@georgebrock.com>
2013-11-29 15:56:03 +0100

    Release version 0.2

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gitsh < Formula
   homepage 'http://thoughtbot.github.io/gitsh/'
-  url 'http://thoughtbot.github.io/gitsh/gitsh-0.1.tar.gz'
-  sha1 '42d4fb2e4f0db4d54fdc47a61bfdfbcf4294682d'
+  url 'http://thoughtbot.github.io/gitsh/gitsh-0.2.tar.gz'
+  sha1 '5618056dcf1b9a496a47ca6016f07086fb605a2b'
 
   system_ruby_version = `ruby -e "puts RUBY_VERSION"`.chomp
   if system_ruby_version < '2.0.0'
