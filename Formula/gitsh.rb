4b77add876cbb8a9e54a3b6afbc2b748fce77ef2
George Brocklehurst <george@georgebrock.com>
2014-02-01 01:26:41 +0100

    gitsh: Mountain Lion compatibility fixes
    Details here: https://github.com/thoughtbot/gitsh/commit/4fd3027

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -1,16 +1,23 @@
 require 'formula'
 
 class Gitsh < Formula
+  SYSTEM_RUBY_PATH = '/usr/bin/ruby'
+
   homepage 'http://thoughtbot.github.io/gitsh/'
   url 'http://thoughtbot.github.io/gitsh/gitsh-0.3.tar.gz'
   sha1 '7f3a4e41f8bf20da3c6e7f7309dd1cba902be957'
 
-  system_ruby_version = `/usr/bin/ruby -e "puts RUBY_VERSION"`.chomp
-  if system_ruby_version < '2.0.0'
+  def self.old_system_ruby?
+    system_ruby_version = `#{SYSTEM_RUBY_PATH} -e "puts RUBY_VERSION"`.chomp
+    system_ruby_version < '2.0.0'
+  end
+
+  if old_system_ruby?
     depends_on 'Ruby'
   end
 
   def install
+    set_ruby_path
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -20,5 +27,15 @@ class Gitsh < Formula
 
   test do
     system "#{bin}/gitsh", "--version"
+  end
+
+  private
+
+  def set_ruby_path
+    if self.class.old_system_ruby?
+      ENV['RUBY'] = "#{HOMEBREW_PREFIX}/bin/ruby"
+    else
+      ENV['RUBY'] = SYSTEM_RUBY_PATH
+    end
   end
 end
