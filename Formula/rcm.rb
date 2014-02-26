b0396c2562eac763f57a0758800cc3bb5bd79fbd
Simon Walker <s.r.walker101@googlemail.com>
2014-02-26 19:54:26 +0000

    Adds head repository

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -4,6 +4,7 @@ class Rcm < Formula
   homepage 'http://thoughtbot.github.io/rcm'
   url 'https://github.com/thoughtbot/rcm/archive/v1.2.0.tar.gz'
   sha1 'b512a2f3d4d81e726e685c4807c4167c232fb09c'
+  head 'https://github.com/thoughtbot/rcm.git'
 
   depends_on "autoconf"
   depends_on "automake"
