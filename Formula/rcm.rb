6785ecbfb657297058ae9e58cc8f557a4508a06d
Mike Burns <mike@mike-burns.com>
2014-03-07 16:27:57 +0100

    rcm: Release version 1.2.1

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -2,16 +2,10 @@ require 'formula'
 
 class Rcm < Formula
   homepage 'http://thoughtbot.github.io/rcm'
-  url 'https://github.com/thoughtbot/rcm/archive/v1.2.0.tar.gz'
-  sha1 'b512a2f3d4d81e726e685c4807c4167c232fb09c'
-
-  depends_on "autoconf"
-  depends_on "automake"
+  url 'http://thoughtbot.github.io/rcm/dist/rcm-1.2.1.tar.gz'
+  sha1 '434bdfbb1df8e610586cd9042a1e2ac614290ca7'
 
   def install
-    system "aclocal"
-    system "automake --add-missing --copy"
-    system "autoconf"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
