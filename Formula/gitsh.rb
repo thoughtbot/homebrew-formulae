800843ac524ebd417421a1c88629bf49fd6653e5
George Brocklehurst <george@georgebrock.com>
2014-04-04 10:49:22 +0200

    gitsh: Release version 0.6

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -5,8 +5,8 @@ class Gitsh < Formula
   HOMEBREW_RUBY_PATH = "#{HOMEBREW_PREFIX}/bin/ruby"
 
   homepage 'http://thoughtbot.github.io/gitsh/'
-  url 'http://thoughtbot.github.io/gitsh/gitsh-0.5.tar.gz'
-  sha1 '1221ab0c0eeaed7544abf91f9a9851f477b00bea'
+  url 'http://thoughtbot.github.io/gitsh/gitsh-0.6.tar.gz'
+  sha1 'e440a68974cdcba68028ef7db5d55d0c2d7a7d79'
 
   def self.old_system_ruby?
     system_ruby_version = `#{SYSTEM_RUBY_PATH} -e "puts RUBY_VERSION"`.chomp
