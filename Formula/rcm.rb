91c5d99b7512843f2787c6ef7a64041596f50127
Mike Burns <mike@mike-burns.com>
2014-05-09 14:30:57 +0200

    rcm: Release version 1.2.3

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rcm < Formula
   homepage 'http://thoughtbot.github.io/rcm'
-  url 'http://thoughtbot.github.io/rcm/dist/rcm-1.2.2.tar.gz'
-  sha1 '844f4325e1eadeb2cf838629e5533b8d7450d0d9'
+  url 'http://thoughtbot.github.io/rcm/dist/rcm-1.2.3.tar.gz'
+  sha1 'f89e64f45d714567f54474b3b4cc825b130ab114'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
