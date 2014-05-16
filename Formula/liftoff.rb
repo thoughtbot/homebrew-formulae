9f636fa4c130ebc518a9cc73c43b76a8bbe036de
Gordon Fontenot <gordon@thoughtbot.com>
2014-05-16 14:18:05 -0400

    liftoff: Release version 1.3

--- Formula/liftoff.rb
+++ Formula/liftoff.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Liftoff < Formula
   homepage 'https://github.com/thoughtbot/liftoff'
-  url 'http://thoughtbot.github.io/liftoff/Liftoff-1.2.tar.gz'
-  sha1 'dd6227604dc5d6023d215c762770d17f4d67b088'
+  url 'http://thoughtbot.github.io/liftoff/Liftoff-1.3.tar.gz'
+  sha1 '5aba430a825b1e93375bf65cf48f7c1fe196d956'
 
   depends_on 'xcproj' => :recommended
 
