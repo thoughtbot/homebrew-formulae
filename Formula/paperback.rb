31f79b65050aa61d2c1896f7af7afebcefcd3d87
Bernerd Schaefer <bj.schaefer@gmail.com>
2014-12-12 10:25:14 -0800

    Update paperback.rb

--- Formula/paperback.rb
+++ Formula/paperback.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Paperback < Formula
   homepage ""
-  version "0.1"
+  version "0.1.1"
 
   if Hardware.is_64_bit?
-    url "https://s3.amazonaws.com/homebrew-formulae/go-paperback_v0.1_darwin_amd64.tar.gz"
-    sha256 "5a65ee4919cb927b2bcecf3271163c753c7b6cd16f2df86cd387005b79e0b257"
+    url "https://s3.amazonaws.com/homebrew-formulae/go-paperback_v0.1.1_darwin_amd64.tar.gz"
+    sha256 "4d143ffc7333b5ec25d555ef99f69c0b8602b9a4f3104fcdaea34ffb35002aaf"
   end
 
   depends_on :arch => :intel
