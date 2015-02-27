60afac6807473d9288be6f899030da12c9721027
Carl Olof Erlandsson <calle@thoughtbot.com>
2015-02-27 17:21:47 +0100

    pick: Correct sha for version 1.1.0

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -3,7 +3,7 @@ require "formula"
 class Pick < Formula
   homepage "http://thoughtbot.github.io/pick/"
   url "https://github.com/thoughtbot/pick/releases/download/v1.1.0/pick-1.1.0.tar.gz"
-  sha1 "cec167acc776065c055ab13a918d4e0300dca2f8"
+  sha1 "8b035e64cc5faea407e73cce01ca039e3ce3b4ac"
 
   def install
     system "./configure", "--prefix=#{prefix}"
