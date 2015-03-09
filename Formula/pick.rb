302a2049e6ad43e94e67baa0cecdc21d255e322d
Carl Olof Erlandsson <calle@thoughtbot.com>
2015-03-09 11:56:29 +0100

    pick: Release version 1.1.1

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Pick < Formula
   homepage "http://thoughtbot.github.io/pick/"
-  url "https://github.com/thoughtbot/pick/releases/download/v1.1.0/pick-1.1.0.tar.gz"
-  sha1 "8b035e64cc5faea407e73cce01ca039e3ce3b4ac"
+  url "https://github.com/thoughtbot/pick/releases/download/v1.1.1/pick-1.1.1.tar.gz"
+  sha1 "6197e968c44e496fcce361936766e5d5150eaa49"
 
   def install
     system "./configure", "--prefix=#{prefix}"
