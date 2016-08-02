5c41120ceacc40e0ca4bc1b77bd79500c39066d4
Fladson Gomes <fladsonthiago@gmail.com>
2016-08-02 16:16:04 -0300

    Fix deprecated is_64_bit

--- Formula/paperback.rb
+++ Formula/paperback.rb
@@ -4,7 +4,7 @@ class Paperback < Formula
   homepage ""
   version "0.1.1"
 
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url "https://s3.amazonaws.com/homebrew-formulae/go-paperback_v0.1.1_darwin_amd64.tar.gz"
     sha256 "4d143ffc7333b5ec25d555ef99f69c0b8602b9a4f3104fcdaea34ffb35002aaf"
   end
