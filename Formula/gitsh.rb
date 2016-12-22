6ca19530c3b350668d6683b735114e43397017d9
George Brocklehurst <george@georgebrock.com>
2016-12-22 17:33:17 +0000

    gitsh: Release version 0.11

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -4,18 +4,19 @@ class Gitsh < Formula
   SYSTEM_RUBY_PATH = '/usr/bin/ruby'
   HOMEBREW_RUBY_PATH = "#{HOMEBREW_PREFIX}/bin/ruby"
 
-  homepage 'https://github.com/thoughtbot/gitsh'
-  url 'http://thoughtbot.github.io/gitsh/gitsh-0.10.tar.gz'
-  sha256 '538558c2f385efc6130ef4e412776055820417c5c87483dbf3568bf31f1f09e3'
+  homepage 'https://github.com/thoughtbot/gitsh/'
+  url 'http://thoughtbot.github.io/gitsh/gitsh-0.11.tar.gz'
+  sha256 '9a2323b7677c4f8f496e3a62913c219654703370ce96efbdb4f12c3192f6218e'
 
   def self.old_system_ruby?
     system_ruby_version = `#{SYSTEM_RUBY_PATH} -e "puts RUBY_VERSION"`.chomp
-    system_ruby_version < '1.9.3'
+    system_ruby_version < '2.0.0'
   end
 
   if old_system_ruby?
     depends_on 'Ruby'
   end
+  depends_on 'readline'
 
   def install
     set_ruby_path
