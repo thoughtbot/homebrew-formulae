2155623186c5ac12f876bda2db8d74539c2aa1c2
George Brocklehurst <george@georgebrock.com>
2016-12-23 22:36:23 +0000

    gitsh: Use env=std by default.
    This causes Homebrew to set the appropriate LDFLAGS for Readline, which
    is a keg only formula.
    
    Without this, gitsh can't build against system Ruby (although it still
    worked on systems where Homebrew's ruby formula was installed).
    
    I couldn't find good documentation for the difference between `env=std` and
    `env=superenv`, but this is where the necessary LDFLAGS value is set:
    
      https://github.com/Homebrew/brew/blob/89fd34b/Library/Homebrew/build.rb#L100

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -4,6 +4,7 @@ class Gitsh < Formula
   SYSTEM_RUBY_PATH = '/usr/bin/ruby'
   HOMEBREW_RUBY_PATH = "#{HOMEBREW_PREFIX}/bin/ruby"
 
+  env :std
   homepage 'https://github.com/thoughtbot/gitsh/'
   url 'http://thoughtbot.github.io/gitsh/gitsh-0.11.tar.gz'
   sha256 '9a2323b7677c4f8f496e3a62913c219654703370ce96efbdb4f12c3192f6218e'
