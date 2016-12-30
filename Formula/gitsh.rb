a1a1261b87d398ff729d08bdaf0748ef9eae4e07
George Brocklehurst <george@georgebrock.com>
2016-12-30 23:45:52 +0000

    gitsh: Use env=std by default.
    Oops, I cut another release without merging
    https://github.com/thoughtbot/gitsh/pull/277 and had to apply this fix by
    hand. Again.

--- Formula/gitsh.rb
+++ Formula/gitsh.rb
@@ -4,6 +4,7 @@ class Gitsh < Formula
   SYSTEM_RUBY_PATH = '/usr/bin/ruby'
   HOMEBREW_RUBY_PATH = "#{HOMEBREW_PREFIX}/bin/ruby"
 
+  env :std
   homepage 'https://github.com/thoughtbot/gitsh/'
   url 'http://thoughtbot.github.io/gitsh/gitsh-0.11.1.tar.gz'
   sha256 'a3e868d140e9972b20c022ff43c761d2643026b10e2c6f67f0ccf47f891a4d6f'
