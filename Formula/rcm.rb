7dd0ea17bfa7d50ec349050c110c09cd2b0ae3ed
Mike Burns <mburns@thoughtbot.com>
2018-07-13 11:36:56 -0400

    rcm: Release version 1.3.2

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -1,8 +1,8 @@
 class Rcm < Formula
   desc "management suite for dotfiles"
   homepage "https://thoughtbot.github.io/rcm"
-  url "https://thoughtbot.github.io/rcm/dist/rcm-1.3.1.tar.gz"
-  sha256 "9c8f92dba63ab9cb8a6b3d0ccf7ed8edf3f0fb388b044584d74778145fae7f8f"
+  url "https://thoughtbot.github.io/rcm/dist/rcm-1.3.2.tar.gz"
+  sha256 "2f31b49c241ed6e74578fc71e6b2ba3a87183d311f28e05b9f7cc7c1c55dda64"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
