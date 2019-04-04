e997e513e11ea670efb1511463359f04052952b9
Geoff Harcourt <geoff.harcourt@gmail.com>
2019-04-03 20:55:16 -0400

    Bump Parity 3.1.0 -> 3.2.0

--- Formula/parity.rb
+++ Formula/parity.rb
@@ -3,8 +3,8 @@ require "formula"
 class Parity < Formula
   homepage "https://github.com/thoughtbot/parity"
   head "https://github.com/thoughtbot/parity.git"
-  sha256 "c804bca89d99f8cfacde6098959a198b9ef347ad8ce40759133fd766206d7b04"
-  url "https://github.com/thoughtbot/parity/archive/3.1.0.tar.gz"
+  sha256 "fb4d72e7d84d7849c25589bf718580b46117f992f2b8b9d22a208bfad6d60a19"
+  url "https://github.com/thoughtbot/parity/archive/v3.2.0.tar.gz"
 
   depends_on "git"
   depends_on "heroku/brew/heroku" => :recommended
