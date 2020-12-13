a6bb452f4c03204fc49556c81e64172700a6d6d1
Joshua Clayton <joshua.clayton@gmail.com>
2020-12-12 21:54:16 -0500

    Release complexity 0.3.0
    What?
    =====
    
    This bumps the version of complexity to 0.3.0.
    
    Additionally, this adds support for building complexity HEAD.

--- Formula/complexity.rb
+++ Formula/complexity.rb
@@ -1,13 +1,13 @@
 class Complexity < Formula
   desc "Identify complex code"
   homepage "https://github.com/thoughtbot/complexity"
-  url "https://github.com/thoughtbot/complexity/archive/0.2.0.tar.gz"
-  sha256 "47e53e0d4705683bfb596038478ce08c361a4c590253514468376148db981300"
+  url "https://github.com/thoughtbot/complexity/archive/0.3.0.tar.gz"
+  sha256 "d46d060cc6981fd3071064eb9b493466bea4a152ed1c9bfed4f7afc1677327da"
   license "MIT"
+  head "https://github.com/thoughtbot/complexity.git"
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
-
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
